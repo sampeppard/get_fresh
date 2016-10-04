7c4f9008645845ee58eff08cb0c7079cd1585bfb
sp <sampeppard@gmail>
2016-10-03 20:03:28 -0400

    add initial routes for stylists to get and create

--- app/app.php
+++ app/app.php
@@ -1,19 +1,35 @@
 <?php
 
-    require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../vendor/Stylist.php";
-    require_once __DIR__."/../vendor/Client.php";
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
 
-    $server = "mysql:host=localhost:8889;dbname=to_do";
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
+
+    $server = "mysql:host=localhost:8889;dbname=hair_salon";
     $username = "root";
     $password = "root";
     $DB = new PDO($server, $username, $password);
 
-    $app = bew Silex\Application();
+    $app = new Silex\Application();
 
-    $app->register(new Silex\Provider\TwigServiceProvider(), array(
-        "twig.path" => __DIR__."/../views"
-    ));
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get("/", function() use ($app) {
+        return $app["twig"]->render("index.html.twig", array("stylists" => Stylist::getAll()));
+    });
+
+    $app->post("/stylists", function() use ($app) {
+        $new_stylist = new Stylist($_POST["name"]);
+        $new_stylist->save();
+        return $app["twig"]->render("index.html.twig", array("stylists" => Stylist::getAll()));
+    });
 
     return $app;
 
--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-    $website = require_once __DIR__.'/../app/app.php';
+    $website = require_once __DIR__."/../app/app.php";
     $website->run();
-
+    
 ?>
